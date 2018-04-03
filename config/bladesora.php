9d2fbe40402befaf71447584f287fe2f39b9d132
calebfavor <calebfavor@gmail.com>
2018-04-03 13:00:11 -0700

    Adding initial laravel package files

---
+++ config/bladesora.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * This should return an array.
+ */
+
+return [
+    'my-settings' => [
+        'setting-1' => true,
+    ],
+];
---
+++ src/Providers/BladesoraServiceProvider.php
@@ -0,0 +1,32 @@
+<?php namespace Railroad\Bladesora\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class BladesoraServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        $this->publishes(
+            [
+                __DIR__ . '/../config/bladesora.php' => config_path('bladesora.php'),
+            ]
+        );
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
