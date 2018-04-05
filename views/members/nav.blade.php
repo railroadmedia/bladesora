3081f285850a21f63cd20de0c7f0df36732894f1
Curtis <curtis@drumeo.com>
2018-04-05 16:02:34 -0700

    Added the symlink and created a nav component. Views are passed to containing project properly and works amazingly.

---
+++ views/members/nav.blade.php
@@ -0,0 +1,8 @@
+<div id="nav" class="container fluid collapsed">
+    <div class="flex flex-row">
+        <div class="flex flex-column logo ph">
+            <img src="{{ $logo }}">
+        </div>
+
+    </div>
+</div>
