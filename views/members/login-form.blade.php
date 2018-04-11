b1bbc79d0919c44dbb13009cbe3514eae3810f4c
Curtis <curtis@drumeo.com>
2018-04-11 09:14:42 -0700

    Login form basically finished. Implemented on Recordeo, could possbily use a few tweaks.

--- views/members/login-form.blade.php
+++ views/members/login-form.blade.php
@@ -1,23 +1,43 @@
-<div id="loginForm" class="flex flex-column shadow bg-white pa-3 mb-3 corners-3">
+<section id="loginForm" class="flex flex-column shadow bg-white pa-3 mb-3 corners-3">
 
-    <div class="form-group mb">
-        <input id="loginEmail" type="email">
-        <label for="loginEmail" class="{{ $brand ?? '' }}">Email Address</label>
-    </div>
+    <form action="{{ $loginUrl }}" class="flex flex-column">
+        <div class="form-group mb">
+            <input id="loginEmail" type="email" name="email">
+            <label for="loginEmail" class="{{ $brand ?? '' }}">Email Address</label>
+        </div>
 
-    <div class="form-group mb">
-        <input id="loginPassword" type="password">
-        <label for="loginPassword" class="{{ $brand ?? '' }}">Password</label>
-    </div>
+        <div class="form-group mb">
+            <input id="loginPassword" type="password" name="password">
+            <label for="loginPassword" class="{{ $brand ?? '' }}">Password</label>
+        </div>
 
-    <button type="submit" class="text-white btn bg-recordeo mb-3">
-        Sign In
-    </button>
+        <button type="submit" class="text-white btn bg-recordeo mb-3">
+            Sign In
+        </button>
+    </form>
 
-    <a class="body text-center text-light">Forgot your password?</a>
-</div>
+    <a id="resetToggle" class="body text-center text-light">Forgot your password?</a>
+</section>
+
+<section id="resetForm" class="flex flex-column shadow bg-white pa-3 mb-3 corners-3 hide">
+
+    <p class="body mb-3 text-light">Please enter your email address and we will send you instructions to reset your password.</p>
+
+    <form action="{{ $resetUrl }}" class="flex flex-column">
+        <div class="form-group mb">
+            <input id="resetEmail" type="email" name="email">
+            <label for="resetEmail" class="{{ $brand ?? '' }}">Email Address</label>
+        </div>
+
+        <button type="submit" class="text-white btn bg-recordeo mb-3">
+            Get New Password
+        </button>
+    </form>
+
+    <a id="loginToggle" class="body text-center text-light">Back to Login</a>
+</section>
 
 <p class="body">
     <strong>Not a member yet?</strong>
-    <a href="">Join the community here!</a>
+    <a href="{{ $joinUrl }}">Join the community here!</a>
 </p>
