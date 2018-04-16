20c29aa0e05d52726eef206004ce12e90215fd2c
calebfavor <calebfavor@gmail.com>
2018-04-16 10:48:17 -0700

    Adding form method type and csrf token to login form.

--- views/members/login-form.blade.php
+++ views/members/login-form.blade.php
@@ -1,6 +1,10 @@
 <section id="loginForm" class="flex flex-column shadow bg-white pa-3 mb-3 corners-3">
 
-    <form action="{{ $loginUrl }}" class="flex flex-column">
+    <form method="post" action="{{ $loginUrl }}" class="flex flex-column">
+        @if($useCsrfToken ?? true)
+            {{ csrf_field() }}
+        @endif
+
         <div class="form-group mb">
             <input id="loginEmail" type="email" name="email">
             <label for="loginEmail" class="{{ $brand ?? '' }}">Email Address</label>
@@ -21,9 +25,14 @@
 
 <section id="resetForm" class="flex flex-column shadow bg-white pa-3 mb-3 corners-3 hide">
 
-    <p class="body mb text-light">Please enter your email address and we will send you instructions to reset your password.</p>
+    <p class="body mb text-light">Please enter your email address and we will send you instructions to reset your
+        password.</p>
 
-    <form action="{{ $resetUrl }}" class="flex flex-column">
+    <form method="post" action="{{ $resetUrl }}" class="flex flex-column">
+        @if($useCsrfToken ?? true)
+            {{ csrf_field() }}
+        @endif
+
         <div class="form-group mb">
             <input id="resetEmail" type="email" name="email">
             <label for="resetEmail" class="{{ $brand ?? '' }}">Email Address</label>
