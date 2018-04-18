40740329be4bc19a9f3f674a63bcafb8abefa043
Curtis <curtis@drumeo.com>
2018-04-18 15:45:24 -0700

    Added a congratulations message to the progress component. Some small alignment and layout changes to the content-info component.

--- views/members/content/content-progress.blade.php
+++ views/members/content/content-progress.blade.php
@@ -1,23 +1,39 @@
 <div class="container mt-3">
-    <div class="flex flex-row flex-wrap shadow bg-{{ $brand }} corners-3 pa">
-        <div class="flex flex-column xs-12 sm-9 lg-10">
-            <div class="flex flex-row trophy-progress-bar mr-2">
-                <div class="flex flex-column trophy-progress-cutoff bg-{{ $brand }} inverted">
-                    <div class="trophy-progress bg-white" style="width:{{ $progress }}%;"></div>
-                </div>
-                <div class="flex flex-column align-center trophy title">
-                    <i class="fas fa-trophy text-{{ $brand }} pr-1"></i>
+    <div class="flex flex-column shadow">
+        <div class="flex flex-row flex-wrap bg-{{ $brand }} corners-3 pa">
+            <div class="flex flex-column xs-12 sm-9 lg-10">
+                <div class="flex flex-row trophy-progress-bar mr-2 {{ $progress === 100 ? 'complete' : '' }}">
+                    <div class="flex flex-column trophy-progress-cutoff bg-{{ $brand }} inverted">
+                        <div class="trophy-progress bg-white" style="width:{{ $progress }}%;"></div>
+                    </div>
+                    <div class="flex flex-column align-center trophy title">
+                        <i class="fas fa-trophy text-{{ $brand }} pr-1"></i>
+                    </div>
                 </div>
             </div>
+            <div class="flex flex-column xs-12 sm-3 lg-2">
+                <a href="{{ $nextLessonUrl }}" class="btn bg-white text-{{ $brand }} short">
+                    @if($progress > 0)
+                        Next Lesson &raquo;
+                    @else
+                        Start {{ ucwords($contentType) }}
+                    @endif
+                </a>
+            </div>
         </div>
-        <div class="flex flex-column xs-12 sm-3 lg-2">
-            <a href="{{ $nextLessonUrl }}" class="btn bg-white text-{{ $brand }} short">
-                @if($progress > 0)
-                    Next Lesson &raquo;
-                @else
-                    Start {{ ucwords($contentType) }}
-                @endif
-            </a>
-        </div>
+        @if($progress === 100)
+            <div class="flex flex-row bg-white corners-bottom-3 pa bb-recordeo-3">
+                <div class="flex flex-column flex-auto align-center">
+                    <div class="square bg-black rounded">
+                        <div class="pa-1 heading text-white">
+                            <i class="fas fa-trophy"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-column align-v-center ph">
+                    <h4 class="subtitle"><span class="font-bold">Course Complete!</span> Congratulations you've finished this Course!</h4>
+                </div>
+            </div>
+        @endif
     </div>
 </div>
