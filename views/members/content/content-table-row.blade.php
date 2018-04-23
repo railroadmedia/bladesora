bfbf9b700b561f56ec979d082e89e8f27f8b5139
calebfavor <calebfavor@gmail.com>
2018-04-23 11:24:42 -0700

    Content table row variables are no longer nested inside an $item array.

--- views/members/content/content-table-row.blade.php
+++ views/members/content/content-table-row.blade.php
@@ -1,21 +1,21 @@
-<a href="{{ $item['url'] }}" class="content-table-row flex flex-row bt-light-1 no-decoration">
+<a href="{{ $url }}" class="content-table-row flex flex-row bt-light-1 no-decoration">
     <div class="flex flex-column thumbnail align-v-center pl-1">
         <div class="thumb-wrap">
-            <div class="thumb-img widescreen bg-center corners-3" style="background-image:url({{ $item['thumbnail'] }});"></div>
+            <div class="thumb-img widescreen bg-center corners-3" style="background-image:url({{ $thumbnail }});"></div>
         </div>
     </div>
 
     <div class="flex flex-column align-v-center pl-1 title-column overflow">
-        @if(!empty($item['colorTitle']))
+        @if(!empty($colorTitle))
             <p class="tiny text-recordeo text-truncate">
-                {{ ucwords($item['colorTitle']) }}
+                {{ ucwords($colorTitle) }}
             </p>
         @endif
         <p class="tiny text-black font-bold text-truncate">
-            {{ $item['title'] }}
+            {{ $title }}
         </p>
         <p class="tiny text-dark text-truncate font-italic hide-md-up">
-            @foreach($item['columnData'] as $index => $columnData)
+            @foreach($columnData as $index => $columnData)
                 @if($index > 0)
                     <span style="margin:0 5px;">&#x25AA;</span>
                 @endif
@@ -25,7 +25,7 @@
         </p>
     </div>
 
-    @foreach($item['columnData'] as $columnData)
+    @foreach($columnData as $columnData)
         <div class="flex flex-column align-center basic-col text-dark font-italic tiny hide-sm-down">
             {{ $columnData }}
         </div>
@@ -33,11 +33,11 @@
 
     <div class="flex flex-column icon-col align-v-center">
         <div class="square body">
-            @if(!empty($item['progress']))
-                <i class="fas {{ $item['progress'] == 'completed' ? 'fa-check-circle' : 'fa-adjust' }} flex-center text-recordeo rounded"></i>
+            @if(!empty($progress))
+                <i class="fas {{ $progress == 'completed' ? 'fa-check-circle' : 'fa-adjust' }} flex-center text-recordeo rounded"></i>
             @else
 
-                @if(in_array($item['contentType'], ['course', 'learning-path', 'pack', 'pack-bundle']))
+                @if(in_array($contentType, ['course', 'learning-path', 'pack', 'pack-bundle']))
                     <i class="fas fa-arrow-circle-right flex-center text-light rounded"></i>
                 @else
                     <i class="fas fa-play-circle flex-center text-light rounded"></i>
