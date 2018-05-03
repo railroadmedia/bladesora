f88f6001a52e27b0ddf6f9b38d7829ffece7a0fa
calebfavor <calebfavor@gmail.com>
2018-05-03 10:15:48 -0700

    Fixing helper commands getting double loaded.

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,22 +1,25 @@
 <?php
 
-function get_resource_icon($filename){
-    $extension = pathinfo($filename, PATHINFO_EXTENSION);
+if (!function_exists('get_resource_icon')) {
+    function get_resource_icon($filename)
+    {
+        $extension = pathinfo($filename, PATHINFO_EXTENSION);
 
-    switch($extension){
-        case 'png':
-            return 'fa-file-image';
-        case 'pdf':
-            return 'fa-file-pdf';
-        case 'zip':
-            return 'fa-file-zip';
-        case 'mp3':
-            return 'fa-file-audio';
-        case 'wav':
-            return 'fa-file-audio';
-        case 'mp4':
-            return 'fa-file-video';
-        default:
-            return 'fa-cloud-download';
+        switch ($extension) {
+            case 'png':
+                return 'fa-file-image';
+            case 'pdf':
+                return 'fa-file-pdf';
+            case 'zip':
+                return 'fa-file-zip';
+            case 'mp3':
+                return 'fa-file-audio';
+            case 'wav':
+                return 'fa-file-audio';
+            case 'mp4':
+                return 'fa-file-video';
+            default:
+                return 'fa-cloud-download';
+        }
     }
 }
