44bfa3e99a205665c6dc1c3fda3b5fafa4ed980b
Curtis <curtis@drumeo.com>
2018-07-26 15:36:32 -0700

    Added in a url check to the subnav to make items active if you're on that section.

--- views/members/navigation/subnav.blade.php
+++ views/members/navigation/subnav.blade.php
@@ -3,7 +3,8 @@
         @foreach($subSections as $section => $info)
             <a href="{{ $info['url'] }}"
                class="flex flex-column subnav-link pa-1 tiny font-bold uppercase align-center text-grey-4 no-decoration br-grey-4-1
-                    {{ strtolower(str_replace(' ', '-', $section)) }}">
+                    {{ strtolower(str_replace(' ', '-', $section)) }}
+                    {{ stripos(url()->current(), $section) !== false ? 'active' : '' }}">
                 <i class="{{ $info['icon'] }}" style="font-size:20px;"></i>
                 <span class="hide-xs-only">{{ $section }}</span>
             </a>
