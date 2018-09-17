93adfa2bb40435128446a93e6daff01c889e8e37
Curtis <curtis@drumeo.com>
2018-09-17 13:26:04 -0700

    Changed isset to !empty to remove it if its 0.. Also turned it into the dense font.

--- views/members/navigation/nav.blade.php
+++ views/members/navigation/nav.blade.php
@@ -12,9 +12,9 @@
             <a href="{{ $links['Notifications']['url'] }}"
                id="notificationButton" class="square">
                 <div class="pa-1 wrap">
-                    @if (isset($unreadNotificationCount))
-                        <span class="rounded bg-{{ $brand }} text-white new">
-                            {!! $unreadNotificationCount <= 99 ? '&nbsp;' . $unreadNotificationCount . '&nbsp;' : $unreadNotificationCount !!}
+                    @if (!empty($unreadNotificationCount))
+                        <span class="rounded bg-{{ $brand }} dense text-white new">
+                            {{ $unreadNotificationCount }}
                         </span>
                     @endif
                     <i class="rounded inset-border text-black fas fa-bell flex-center"></i>
