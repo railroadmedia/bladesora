b26419406ef0d55ee98fb8799aea54bc161f1a20
Curtis <curtis@drumeo.com>
2018-09-21 11:01:27 -0700

    Added in the custom class directive to the textarea input.

--- views/members/inputs/textarea-input.blade.php
+++ views/members/inputs/textarea-input.blade.php
@@ -1,10 +1,12 @@
 <div class="form-group">
     <textarea id="{{ $inputId }}"
               name="{{ $inputName }}"
-              class="{{ !empty($inputErrors) && count($inputErrors) > 0 ? 'has-error' : '' }}"
+              class="{{ !empty($inputErrors) && count($inputErrors) > 0 ? 'has-error' : '' }} {{ $customClass ?? '' }}"
             {{ !empty($disabled) && $disabled === true ? 'disabled' : '' }}>{{ $inputValue ?? '' }}</textarea>
-    <label for="{{ $inputId }}"
-           class="{{ $brand }}">{{ $inputLabel }}</label>
+    @if(!empty($inputLabel))
+        <label for="{{ $inputId }}"
+               class="{{ $brand }}">{{ $inputLabel }}</label>
+    @endif
 
     @include('bladesora::members.inputs.partials._errors', [
         "inputErrors" => $inputErrors
