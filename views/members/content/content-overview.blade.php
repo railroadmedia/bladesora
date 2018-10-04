c0f8967521f77e11129f687100ec81f7eed2de87
Curtis <curtis@drumeo.com>
2018-10-04 14:37:28 -0700

    Mossing a corner modifier.

--- views/members/content/content-overview.blade.php
+++ views/members/content/content-overview.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-row pv-3 content-overview {{ !empty($hideBorder) && $hideBorder ? '' : 'bt-grey-1-1' }}">
     <div class="flex flex-column align-v-center large-thumbnail {{ !empty($showBackgroundCards) && $showBackgroundCards === true ? 'background-cards' : '' }} {{ $themeColor }} {{ !empty($releaseDate) && \Carbon\Carbon::parse($releaseDate) > \Carbon\Carbon::now() ? 'desaturate' : '' }}">
-        <div class="thumb-wrap">
+        <div class="thumb-wrap corners-3">
             <a @if((empty($noLink) || $noLink === false) && $isOwned) href="{{ $lessonsUrl }}" @endif>
                 <div class="thumb-img bg-center corners-3 {{ $forceSquareThumb === true ? 'square' : 'widescreen' }}"
                      style="background-image:url({{ $itemThumbnail }});">
