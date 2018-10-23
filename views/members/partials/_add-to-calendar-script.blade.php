2d516e4f631f3b5c16cbb226d85162ab0291775e
Curtis <curtis@drumeo.com>
2018-10-23 12:15:21 -0700

    Add to calendar include script.

---
+++ views/members/partials/_add-to-calendar-script.blade.php
@@ -0,0 +1,28 @@
+<script type="text/javascript" src="https://addevent.com/libs/atc/1.6.1/atc.min.js" async defer></script>
+<script type="text/javascript">
+    window.addeventasync = function () {
+        addeventatc.settings({
+            appleical: {
+                show: true,
+                text: 'Apple Calendar'
+            },
+            google: {
+                show: true,
+                text: 'Google <em>(online)</em>'
+            },
+            outlook: {
+                show: true,
+                text: 'Outlook'
+            },
+            outlookcom: {
+                show: true,
+                text: 'Outlook.com <em>(online)</em>'
+            },
+            yahoo: {
+                show: true,
+                text: 'Yahoo <em>(online)</em>'
+            },
+            dropdown: {order: "appleical,google,outlook,outlookcom,yahoo"}
+        });
+    };
+</script>
