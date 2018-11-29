a5085e44b27f126749f11cc50490adbe1f247901
Bogdan Damian <bogdan.damian@artsoft-consult.ro>
2018-11-29 17:15:48 +0200

    added guitar gear form template for guitareo user profile - PMG-99

---
+++ views/members/account/settings/profile/guitar-gear-form.blade.php
@@ -0,0 +1,140 @@
+@component('bladesora::members.account.settings.edit-form')
+    @slot('formTitle')
+        My Gear
+    @endslot
+
+    @slot('modalId')
+        gearModal
+    @endslot
+
+    @slot('formData')
+        <div class="flex flex-column grow">
+            @include('bladesora::members.account.partials._text-fields', [
+                "fields" => [
+                    "Played Guitar Since" => $playedGuitarSince,
+                    "Guitars" => $guitars,
+                    "Amps" => $amps,
+                    "Pedals" => $pedals,
+                    "Strings" => $strings,
+                    "My Gear Photo" => '',
+                ],
+                "showEmpty" => true
+            ])
+
+            @if($gearPhoto)
+            <div class="flex flex-row bg-center relative" style="max-width:480px;background-image:url({{ $gearPhoto }});">
+                <div class="widescreen bg-center"></div>
+
+                <span id="clearGearPhoto" class="rounded clear-button">
+                    <i class="fas fa-times"></i>
+                </span>
+            </div>
+            @endif
+        </div>
+    @endslot
+
+    @slot('formModal')
+        <div id="gearModal" class="modal">
+            <div class="flex flex-column bg-white corners-3 shadow">
+                <div class="flex flex-row pa-3">
+                    <h2 class="subheading">Edit: My Gear</h2>
+                </div>
+
+                <form method="POST" action="{{ $action }}" enctype="multipart/form-data">
+                    {{ method_field($method) }}
+                    {{ csrf_field() }}
+
+
+                    <div class="flex flex-row ph-3 mb-1">
+                        <div class="flex flex-column">
+                            @include('bladesora::members.inputs.select-input', array_merge([
+                                "brand" => $brand ?? "guitareo",
+                                "inputId" => "playedGuitarSince",
+                                "inputName" => "played_guitar_since",
+                                "inputLabel" => "Played Guitar Since",
+                                "inputValue" => '',
+                                "inputOptions" => array_reverse(range(1900, date('Y'))),
+                                "inputErrors" => [],
+                            ], $playedGuitarSinceInput ?? []))
+                        </div>
+                    </div>
+                    <div class="flex flex-row ph-3 inline-inputs">
+                        <div class="flex flex-column">
+                            @include('bladesora::members.inputs.text-input', array_merge([
+                                "brand" => $brand ?? "guitareo",
+                                "type" => "text",
+                                "inputId" => "guitars",
+                                "inputName" => "guitars",
+                                "inputLabel" => "Guitars",
+                                "inputValue" => "",
+                                "inputErrors" => [],
+                            ], $guitarsInput ?? []))
+                        </div>
+                        <div class="flex flex-column">
+                            @include('bladesora::members.inputs.text-input', array_merge([
+                                "brand" => $brand ?? "guitareo",
+                                "type" => "text",
+                                "inputId" => "amps",
+                                "inputName" => "amps",
+                                "inputLabel" => "Amps",
+                                "inputValue" => "",
+                                "inputErrors" => [],
+                            ], $ampsInput ?? []))
+                        </div>
+                    </div>
+
+                    <div class="flex flex-row ph-3 inline-inputs">
+                        <div class="flex flex-column">
+                            @include('bladesora::members.inputs.text-input', array_merge([
+                                "brand" => $brand ?? "guitareo",
+                                "type" => "text",
+                                "inputId" => "pedals",
+                                "inputName" => "pedals",
+                                "inputLabel" => "Pedals",
+                                "inputValue" => "",
+                                "inputErrors" => [],
+                            ], $pedalsInput ?? []))
+                        </div>
+                        <div class="flex flex-column">
+                            @include('bladesora::members.inputs.text-input', array_merge([
+                                "brand" => $brand ?? "guitareo",
+                                "type" => "text",
+                                "inputId" => "strings",
+                                "inputName" => "strings",
+                                "inputLabel" => "strings",
+                                "inputValue" => "",
+                                "inputErrors" => [],
+                            ], $stringsInput ?? []))
+                        </div>
+                    </div>
+
+                    <div class="flex flex-row ph-3 pv mb-1">
+                        <div class="flex flex-column">
+                            @include('bladesora::members.inputs.file-input', array_merge([
+                                "brand" => $brand ?? "guitareo",
+                                "inputId" => "gearPhoto",
+                                "inputName" => "gear",
+                                "inputLabel" => "Gear Photo",
+                                "inputValue" => "",
+                                "inputErrors" => [],
+                            ], $gearInput ?? []))
+                        </div>
+                    </div>
+
+                    <div class="flex flex-row ph-3 pb-3">
+                        <button class="btn collapse-150 mr-1"
+                                type="submit">
+                            <span class="bg-{{ $brand }} text-white corners-3 short">
+                                Save
+                            </span>
+                        </button>
+
+                        <a class="btn collapse-150 close-modal corners-3 flat text-black flat short">
+                            Cancel
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    @endslot
+@endcomponent
