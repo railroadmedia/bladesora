ff36a2e147f07576e9b9a64184eff60818539e21
Curtis <curtis@drumeo.com>
2018-12-27 13:15:34 -0800

    Removed borders from the subnav items. Added a centering class for desktop.

--- views/members/navigation/subnav.blade.php
+++ views/members/navigation/subnav.blade.php
@@ -1,9 +1,9 @@
 <div id="subNav" class="container fluid collapsed bg-black bb-grey-5-1">
     <div class="container relative {{ count($subSections) > 4 ? 'pad-sides' : 'collapsed' }}">
-        <div id="subNavWrap" class="flex flex-row overflow">
+        <div id="subNavWrap" class="flex flex-row align-h-center overflow">
             @foreach($subSections as $section)
                 <a href="{{ $section['url'] }}"
-                   class="flex flex-column subnav-link pa-1 uppercase align-center text-grey-4 no-decoration br-grey-5-1
+                   class="flex flex-column subnav-link pa-1 uppercase align-center text-grey-4 no-decoration
                     {{ $section['active'] ? 'active' : '' }}">
                     <i class="{{ $section['icon'] }}
                             {{ $section['active'] ? ('text-' . $themeColor) : 'text-grey-4' }}" style="font-size:20px;"></i>
@@ -20,7 +20,7 @@
                 <i class="fas fa-chevron-left"></i>
             </div>
 
-            <div id="scrollSubNavRight" class="scroll-sub-nav body text-grey-3 bh-grey-5-1">
+            <div id="scrollSubNavRight" class="scroll-sub-nav body text-grey-3 bh-grey-5-1 hide">
                 <i class="fas fa-chevron-right"></i>
             </div>
         @endif
