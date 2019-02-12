025d483bb0f69b2615582995e999632588593c66
Curtis <curtis@drumeo.com>
2019-02-12 09:45:10 -0800

    Changed some text color and alignment of breadcrumbs.

--- views/members/navigation/breadcrumbs.blade.php
+++ views/members/navigation/breadcrumbs.blade.php
@@ -1,13 +1,13 @@
 <div id="breadCrumbs" class="container fluid bg-black bb-grey-5-1 collapsed hide-xs-only">
-    <div class="container text-grey-2 pa-1 uppercase">
+    <div class="container text-grey-3 pa-1 uppercase text-center">
         {{--<div class="flex flex-row">--}}
             @foreach($pages as $index => $page)
                 @if($index !== 0)
-                    <span class="mh-1 tiny">&raquo;</span>
+                    <span class="mh-1 tiny">/</span>
                 @endif
 
                 @if(!empty($page['url']))
-                    <a class="tiny text-grey-2 no-decoration" href="{{ $page['url'] }}">
+                    <a class="tiny text-grey-3 no-decoration" href="{{ $page['url'] }}">
                         {{ ucwords($page['title']) }}
                     </a>
                 @else
