bbe118f47e5514a404e6e3dd1083ba2d5027956c
Curtis <curtis@drumeo.com>
2019-02-28 09:07:07 -0800

    Removed hidden input from toggle component. Handling with JS now.

--- views/members/inputs/toggle-input.blade.php
+++ views/members/inputs/toggle-input.blade.php
@@ -1,11 +1,7 @@
 <div class="flex flex-row form-group align-v-center">
     <span class="toggle-input mr-1">
-        {{-- the hidden input will force the checkbox to return false if its not checked --}}
-        <input type="hidden" name="{{ $inputName }}" value="0">
-
         <input id="{{ $inputID }}"
                name="{{ $inputName }}"
-               value="{{ $inputValue ?? 'on' }}"
                {{ !empty($checked) && $checked === true ? 'checked' : '' }}
                type="checkbox">
 
