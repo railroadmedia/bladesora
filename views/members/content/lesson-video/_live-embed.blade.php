7468baa033239d353cff463cfd5954e2ae67d811
jonathan <jonc@drumeo.com>
2019-04-08 10:06:03 -0700

    adding dusk selectors to chatroll

--- views/members/content/lesson-video/_live-embed.blade.php
+++ views/members/content/lesson-video/_live-embed.blade.php
@@ -20,7 +20,7 @@
             ></email-form>
         </div>
     </div>
-    <div id="chatContainer" class="flex flex-column">
+    <div id="chatContainer" class="flex flex-column" dusk="chat-container">
         <div class="inside flex flex-column corners-3 shadow bg-white">
             <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowtransparency="true" src="{{ $chatrollUrl }}"></iframe>
         </div>
