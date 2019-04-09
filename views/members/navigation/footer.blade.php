e3e1a2ce972b179a2c551071a0f11f6becb39bcb
Curt <curtis@drumeo.com>
2019-04-09 14:37:13 -0700

    Add legacy resources to footer

--- views/members/navigation/footer.blade.php
+++ views/members/navigation/footer.blade.php
@@ -4,8 +4,14 @@
             <section class="flex flex-column footer-col">
                 <div class="flex flex-row tiny align-v-center">
                     Musora Media Inc &copy; {{ date('Y') }} -
-                    <a href="{{ $termsUrl }}" target="_blank" class="text-grey-3">Terms</a> /
+                    <a href="{{ $termsUrl }}" target="_blank" class="text-grey-3">Terms</a>
+                    /
                     <a href="{{ $privacyUrl }}" target="_blank" class="text-grey-3">Privacy</a>
+
+                    @if(!empty($legacyResourcesUrl))
+                        /
+                        <a href="{{ $legacyResourcesUrl }}" target="_blank" class="text-grey-3">Legacy Resources</a>
+                    @endif
                 </div>
             </section>
 
@@ -13,7 +19,8 @@
 
             <section class="flex flex-column footer-col">
                 <div class="flex flex-row tiny align-v-center">
-                    <a href="{{ $supportUrl }}" class="text-grey-3">Support</a> /
+                    <a href="{{ $supportUrl }}" class="text-grey-3">Support</a>
+                    /
                     <a href="{{ $logoutUrl }}" class="text-grey-3">Logout</a>
                 </div>
             </section>
