2e29a91506d06d2ca812de4ade08e2f67667db31
Curtis <curtis@drumeo.com>
2019-11-01 15:33:26 -0700

    Add user metric component

---
+++ views/members/components/user-metric.blade.php
@@ -0,0 +1,9 @@
+<div class="pa-1">
+    <div class="user-metric flex flex-column align-center heading ba-grey-1-5 align-center pa-2">
+        <i class="{{ $icon }} text-{{ $themeColor ?? 'drumeo' }} text-center nowrap" style="line-height:32px;"></i>
+
+        <h4 class="large-display text-center nowrap" style="line-height:.8em;">{{ $value }}</h4>
+
+        <h6 class="tiny text-grey-3 text-center dense uppercase nowrap" style="line-height:32px;">{{ $label }}</h6>
+    </div>
+</div>
