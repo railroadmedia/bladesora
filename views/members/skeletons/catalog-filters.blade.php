28cd9534c064dfcef9f2ff83ae16c2ec1f5a1f4a
Curtis <curtis@drumeo.com>
2019-12-01 18:26:18 -0800

    Fix padding on filters

--- views/members/skeletons/catalog-filters.blade.php
+++ views/members/skeletons/catalog-filters.blade.php
@@ -1,9 +1,9 @@
-<div class="flex flex-row flex-wrap nmh-1 ph-1 mb-1">
+<div class="flex flex-row flex-wrap mb-1">
     @for($i = 0; $i < $length; $i++)
-        <div class="flex flex-column xs-12 sm-3 ph-1">
+        <div class="flex flex-column xs-12 sm-3 ph-1 mb-2 m-xs-only">
             <div
-                    class="skeleton-loader corners-3"
-                    style="height:50px;width:100%;"
+                class="skeleton-loader corners-3"
+                style="height:50px;width:100%;"
             ></div>
         </div>
     @endfor
