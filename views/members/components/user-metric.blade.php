0be9d885e29dd396e8fda4a02e881445cef18411
Curtis <curtis@drumeo.com>
2019-12-09 11:06:35 -0800

    Black text

--- views/members/components/user-metric.blade.php
+++ views/members/components/user-metric.blade.php
@@ -2,7 +2,7 @@
     <div class="user-metric flex flex-column align-center heading ba-grey-1-5 align-center pa-2">
         <i class="{{ $icon }} text-{{ $themeColor ?? 'drumeo' }} text-center nowrap mb-1" style="line-height:32px;"></i>
 
-        <h4 class="large-display text-center nowrap" style="line-height:.8em;">{{ $value }}</h4>
+        <h4 class="large-display text-black text-center nowrap" style="line-height:.8em;">{{ $value }}</h4>
 
         <h6 class="tiny text-grey-3 text-center dense uppercase nowrap" style="line-height:32px;">{{ $label }}</h6>
     </div>
