28d20f852cdb7acef273a93714bc8500aab3a478
Bogdan Damian <bogdan.damian@artsoft-consult.ro>
2021-04-08 11:22:00 +0300

    added new chat

--- views/members/content/lesson-video/_live-embed.blade.php
+++ views/members/content/lesson-video/_live-embed.blade.php
@@ -17,12 +17,20 @@
                     theme-color="{{ $brand }}"
                     success-message="Your question has been sent!"
                     :lesson-page="true"
+                    :enable-event="true"
+                    event-name="postQuestion"
             ></email-form>
         </div>
     </div>
-    <div id="chatContainer" class="flex flex-column" dusk="chat-container">
-        <div class="inside flex flex-column corners-3 shadow bg-white">
-            <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowtransparency="true" src="{{ $chatrollUrl }}"></iframe>
-        </div>
+    <div class="inside flex flex-column shadow bg-white" style="height: 690px;">
+        <chat
+            api-key="{{ $apiKey }}"
+            token="{{ $token }}"
+            user-id="{{ current_user()->getId() }}"
+            chat-channel-name="{{ $chatChannelName }}"
+            questions-channel-name="{{ $questionsChannelName }}"
+            :is-administrator="{{ json_encode(boolval($isAdministrator)) }}"
+            :user-data="{{ json_encode($userData) }}"
+        ></chat>
     </div>
 </div>
