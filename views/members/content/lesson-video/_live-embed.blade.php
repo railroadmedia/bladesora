14d2fd2ce388dc3e4d8ede2b589fcbfcf752b2be
calebfavor <calebfavor@gmail.com>
2021-05-06 16:14:45 -0700

    Adding lesson download resources button to live stream title.

--- views/members/content/lesson-video/_live-embed.blade.php
+++ views/members/content/lesson-video/_live-embed.blade.php
@@ -6,6 +6,32 @@
         <div class="video-title pt-1">
             <div class="flex flex-row">
                 <h1 class="flex heading text-white">{{ $lessonTitle }}</h1>
+
+                @if(!empty($lessonResources))
+                    <div class="flex flex-column align-center sq-btn-col mr-2">
+                        <div class="btn bg-{{ $brand }} inverted text-{{ $brand }} is-dropdown"
+                             data-tooltip="Download Resources">
+                            <i class="unopen fas fa-download no-events text-{{ $brand }}"></i>
+                            <i class="open fas fa-download no-events text-white"></i>
+
+                            <div class="dropdown-content bg-white shadow tiny text-black">
+                                <ul>
+                                    @foreach($lessonResources as $resource)
+                                        <li>
+                                            <a class="no-decoration text-black pa-1"
+                                               href="{{ $resource['resource_url'] }}"
+                                               target="_blank"
+                                               download>
+                                                <i class="fas {{ get_resource_icon($resource['resource_url']) }} mr-1" style="width:20px;text-align:center;"></i>  {{ $resource['resource_name'] }}
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+
                 <div class="">
                     <span id="liveIndicator" class="bg-error text-white tiny font-bold corners-3 text-center uppercase pr-1 pl-1">live</span>
                 </div>
