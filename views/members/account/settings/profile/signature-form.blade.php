8eb0a2a3dbc5566e5b7765a7a556ff5140b28cf0
Roxana Riza <roxana_r2000@yahoo.com>
2021-05-12 13:10:16 +0300

    Add user signature profile settings

---
+++ views/members/account/settings/profile/signature-form.blade.php
@@ -0,0 +1,64 @@
+@component('bladesora::members.account.settings.edit-form')
+    @slot('formTitle')
+        Signature
+    @endslot
+
+    @slot('modalId')
+        signatureModal
+    @endslot
+
+    @slot('formData')
+
+        <div class="flex flex-column grow">
+            <p class="body">{{$signature}}</p>
+
+            <p class="tiny text-grey-3 font-italic">
+                This will appear below your posts on the forums page.
+            </p>
+        </div>
+    @endslot
+
+    @slot('formModal')
+        <div id="signatureModal" class="modal">
+            <div class="flex flex-column bg-white corners-3 shadow">
+                <div class="flex flex-row pa-3">
+                    <h2 class="subheading">Edit: Signatue</h2>
+                </div>
+
+                <form method="POST" action="{{ $action }}">
+                    {{ method_field($method) }}
+                    {{ csrf_field() }}
+
+                    <div class="flex flex-row ph-3 mb-1">
+                        <div class="flex flex-column">
+                            <div class="flex flex-row ph-3 mb-1">
+                                <div class="flex flex-column">
+                                    @include('bladesora::members.inputs.textarea-input', [
+                                        "brand" => $brand,
+                                        "inputId" => "signature",
+                                        "inputName" => "signature",
+                                        "inputLabel" => "Signature",
+                                        "inputValue" => $signature,
+                                        "inputErrors" => [],
+                                    ])
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-row ph-3 pb-3">
+                        <button class="btn collapse-150 mr-1">
+                            <span class="bg-{{ $brand }} text-white corners-3 short">
+                                Save
+                            </span>
+                        </button>
+
+                        <a class="btn collapse-150 close-modal corners-3 flat text-black flat short">
+                            Cancel
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    @endslot
+@endcomponent
