2eb943d186ae8a8a342dbd413769ab3bac12203a
Bogdan Damian <bogdan.damian@artsoft-consult.ro>
2021-10-21 18:02:44 +0300

    added updates to 'invite a friend' for users who reached max referrals used - RSWSV10-12

--- views/members/referral/invite.blade.php
+++ views/members/referral/invite.blade.php
@@ -1,7 +1,16 @@
+@php
+    $titleLineOne = $canRefer ? 'Share a free 30-day pass' : 'Thank you for your';
+    $titleLineTwo = $canRefer ? 'with up to five friends.' : 'referrals & support!';
+    $footerLineOne = $canRefer ? 'Guest passes are for new subscribers only and cannot be' : 'You have referred the maximum of five guests.';
+    $footerLineTwo = $canRefer ? 'redeemed for renewals, extensions, or gift subscriptions.' : 'You\'ll have access to more invites soon!';
+
+    $containerClass = $canRefer ? '' : 'tw-justify-center';
+@endphp
+
 <section class="tw-text-center tw-text-white tw-py-6 md:tw-py-10 lg:tw-pt-12 lg:tw-pb-16">
     <div class="tw-container tw-mx-auto tw-max-w-7xl">
-        <h1 class="tw-leading-tight lg:tw-mb-14"><strong>Share a free 30-day pass<br> with up to five friends.</strong></h1>
-        <div class="tw-flex tw-flex-col xl:tw-flex-row tw-items-center tw-px-4">
+        <h1 class="tw-leading-tight lg:tw-mb-14"><strong>{{ $titleLineOne }}<br> {{ $titleLineTwo }}</strong></h1>
+        <div class="tw-flex tw-flex-col xl:tw-flex-row tw-items-center tw-px-4 {{ $containerClass }}">
             <div class="tw-flex-shrink-0 tw-w-full lg:tw-w-auto tw-my-5 md:tw-my-6 lg:tw-my-0">
                 <div class="tw-relative">
                     <img class="tw-inline-block tw-w-full tw-max-w-xs sm:tw-max-w-md md:tw-max-w-lg lg:tw-max-w-xl" src="https://drumeo-assets.s3.amazonaws.com/redeem/referral/30-day-guest-pass.png">
@@ -11,30 +20,31 @@
                     </div>
                 </div>
             </div>
+            @if($canRefer)
+                <div class="tw-flex tw-flex-col lg:tw-h-full lg:tw-pl-10 tw-w-full tw-max-w-lg lg:tw-max-w-none tw-mx-auto">
+                    <h5 class="tw-leading-tight tw-py-4">Give a friend unlimited access to Drumeo, free for 30 days</h5>
+                    <form id="invite-email-form" name="drumeo" method="post" action="{{ $emailInviteUrl }}">
+                        <label class="tw-inline-block tw-w-full tw-text-left tw-pt-6" for="email"><strong>Invite via email</strong></label>
+                        <div class="tw-flex tw-flex-wrap sm:tw-flex-nowrap tw-items-center tw-justify-center tw-mt-1">
+                            <input class="tw-inline-block tw-w-full tw-mb-4 sm:tw-mb-0 sm:tw-mr-4 tw-default-form-field sm:tw-flex-grow tw-pt-0" type="email" id="email" name="email" placeholder="Email address..." value="{{ Input::old('email') }}">
+                            <input name="button" type="submit" id="button" class="tw-bg-drumeo tw-leading-none tw-text-base tw-font-bold tw-border-0 tw-rounded-full tw-select-none tw-cursor-pointer tw-text-center tw-py-4 tw-px-6 tw-uppercase tw-font-roboto tw-text-white tw-flex-none tw-w-52" value="Send Guest Pass"/>
+                        </div>
+                    </form>
 
-            <div class="tw-flex tw-flex-col lg:tw-h-full lg:tw-pl-10 tw-w-full tw-max-w-lg lg:tw-max-w-none tw-mx-auto">
-                <h5 class="tw-leading-tight tw-py-4">Give a friend unlimited access to Drumeo, free for 30 days</h5>
-                <form id="commentform" name="drumeo" method="post" action="{{ URL::route('access-codes.form-claim') }}">
-                    <label class="tw-inline-block tw-w-full tw-text-left tw-pt-6" for="email"><strong>Invite via email</strong></label>
-                    <div class="tw-flex tw-flex-wrap sm:tw-flex-nowrap tw-items-center tw-justify-center tw-mt-1">
-                        <input class="tw-inline-block tw-w-full tw-mb-4 sm:tw-mb-0 sm:tw-mr-4 tw-default-form-field sm:tw-flex-grow tw-pt-0" type="email" id="email" name="email" placeholder="Email address..." value="{{ Input::old('email') }}">
-                        <input name="button" type="submit" id="button" class="tw-bg-drumeo tw-leading-none tw-text-base tw-font-bold tw-border-0 tw-rounded-full tw-select-none tw-cursor-pointer tw-text-center tw-py-4 tw-px-6 tw-uppercase tw-font-roboto tw-text-white tw-flex-none tw-w-52" value="Send Guest Pass"/>
-                    </div>
-                </form>
-
-                <form id="commentform" name="drumeo" method="post" action="{{ URL::route('access-codes.form-claim') }}">
-                    <label class="tw-inline-block tw-w-full tw-text-left tw-pt-6" for="email"><strong>Share your link</strong></label>
-                    <div class="tw-flex tw-flex-wrap sm:tw-flex-nowrap tw-items-center tw-justify-center tw-mt-1">
-                        <input class="tw-inline-block tw-w-full tw-mb-4 sm:tw-mb-0 sm:tw-mr-4 tw-default-form-field sm:tw-flex-grow tw-pt-0" type="email" id="email" name="email" placeholder="link" value="{{ $userReferralLink }}">
-                        <input name="button" type="submit" id="button" class="tw-bg-drumeo tw-leading-none tw-text-base tw-font-bold tw-border-0 tw-rounded-full tw-select-none tw-cursor-pointer tw-text-center tw-py-4 tw-px-6 tw-uppercase tw-font-roboto tw-text-white tw-flex-none tw-w-52" value="Copy Link"/>
-                    </div>
-                </form>
-            </div>
+                    <form id="invite-link-form" name="drumeo" method="post" action="#">
+                        <label class="tw-inline-block tw-w-full tw-text-left tw-pt-6" for="email"><strong>Share your link</strong></label>
+                        <div class="tw-flex tw-flex-wrap sm:tw-flex-nowrap tw-items-center tw-justify-center tw-mt-1">
+                            <input class="tw-inline-block tw-w-full tw-mb-4 sm:tw-mb-0 sm:tw-mr-4 tw-default-form-field sm:tw-flex-grow tw-pt-0" type="email" id="email" name="email" placeholder="link" value="{{ $userReferralLink }}">
+                            <input name="button" type="submit" id="button" class="tw-bg-drumeo tw-leading-none tw-text-base tw-font-bold tw-border-0 tw-rounded-full tw-select-none tw-cursor-pointer tw-text-center tw-py-4 tw-px-6 tw-uppercase tw-font-roboto tw-text-white tw-flex-none tw-w-52" value="Copy Link"/>
+                        </div>
+                    </form>
+                </div>
+            @endif
         </div>
     </div>
 </section>
 <section  class="tw-text-center tw-text-white tw-py-5 md:tw-py-8" style="background-color:#081f35;">
     <div class="tw-container tw-mx-auto tw-max-w-6xl">
-        <h5 class="tw-leading-normal tw-opacity-70">Guest passes are for new subscribers only and cannot be<br class="tw-hidden tw-g:inline"> redeemed for renewals, extensions, or gift subscriptions.</h5>
+        <h5 class="tw-leading-normal tw-opacity-70">{{ $footerLineOne }}<br class="tw-hidden lg:tw-inline"> {{ $footerLineTwo }}</h5>
     </div>
 </section>
