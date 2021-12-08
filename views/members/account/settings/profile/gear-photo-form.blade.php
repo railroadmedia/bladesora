0191b910d4bc156ef7e7712462508fdd97402431
Jonathan Maingot <jonathan@drumeo.com>
2021-12-08 12:27:57 -0800

    Revert "Merge branch 'contact-page-updates' into v9.0-coaches-update"
    This reverts commit 321b0dfd1d75176c08b04bf0f567187e697f4fb9, reversing
    changes made to 08a81743a841eff0f21326ed47ddefc0547c15e2.

--- views/members/account/settings/profile/gear-photo-form.blade.php
+++ views/members/account/settings/profile/gear-photo-form.blade.php
@@ -42,7 +42,7 @@
                     upload-endpoint="{{ $uploadRequestEndpoint }}"
                     save-endpoint="{{ $fieldSaveRequestEndpoint }}"
                     user-id="{{ $userId }}"
-                    @image-uploaded="gearPhotoUploaded"/>
+                    @image-uploaded="gearPhotoUploaded"></image-cropper>
             </div>
         </div>
     @endslot
--- views/members/partials/_contact.blade.php
+++ views/members/partials/_contact.blade.php
@@ -1,54 +1,36 @@
-<div class="flex flex-column tw-max-w-3xl tw-mx-auto tw-px-4">
-
-    <section>
-        <div class="tw-mt-12 tw-text-center">
-            <h2 class="tw-font-bold tw-text-3xl tw-mb-6">We'd love to hear from you!</h2>
-            <p class="tw-mb-6 tw-text-base">Whether your question is about membership, shipping, technical troubles or anything else, our amazing support team is ready to answer any and all of your questions!</p>
-            <p class="tw-text-base">You may find your response in our <a href="https://help.{{ $brand }}.com/" class="tw-font-bold tw-no-underline tw-text-{{ $themeColor }}" title="go to help center">Help Center here</a>, but if not, fill out the quick form below.</p>
+<div class="flex flex-column">
+    <div class="flex flex-row">
+        <div class="flex flex-column xs-12 pa">
+            <h1 class="heading pv">Contact Us</h1>
         </div>
-    </section>
-
-    <section class="flex flex-row flex-wrap mv-3">
-        <div class="flex flex-column">
+    </div>
+    <div class="flex flex-row flex-wrap">
+        <div class="flex flex-column align-v-top pa xs-12 md-8">
 
             {{--TODO: Requires an instance of Vue with the Vuesora plugin--}}
-                <support-email-form 
-                    brand="{{ $brand }}"
+                <email-form
                     email-subject="{{ $emailSubject }}"
+                    theme-color="{{ $themeColor }}"
+                    brand="{{ $brand }}"
+                    recipient="{{ $recipient ?? 'support@' . $brand . '.com' }}"
+                    input-label="{{ $emailInputLabel }}"
                     email-type="{{ $emailType }}"
                     email-endpoint="{{ $emailEndpoint }}"
                     email-logo="{{ $emailLogo }}"
                     email-alert="{{ $emailSubject }}"
-                    input-label="{{ $emailInputLabel }}"
-                    :lesson-page="false"
-                    recipient="{{ $recipient ?? 'support@' . $brand . '.com' }}"
                     success-message="{{ $emailSuccessMessage }}"
-                />
+                    :lesson-page="false"></email-form>
             {{--TODO: Requires an instance of Vue with the Vuesora plugin--}}
         </div>
-    </section>
+        <div class="flex flex-column align-center pa xs-12 md-4">
+            <h4 class="subtitle">International</h4>
+            <a href="tel:{{ $internationalNumber }}" class="title text-{{ $themeColor }} font-light mb-2 no-decoration">{{ $internationalNumber }}</a>
 
-    <section class="tw-flex tw-flex-col tw-text-center tw-my-8">
-        <h2 class="tw-font-bold tw-text-3xl">Old fashioned phone calls work too!</h2>
-        <div class="tw-flex tw-my-8 tw-flex-col tw-items-center sm:tw-flex-row">
-            <div class="tw-flex tw-flex-col tw-w-10/12 sm:tw-w-1/3 tw-font-bold tw-justify-center tw-p-3 tw-border-solid tw-border-0 tw-border-b-2 sm:tw-border-b-0 sm:tw-border-r-2 tw-border-gray-300">
-                <h4 class="tw-font-bold tw-mb-1">Toll-Free</h4>
-                <a href="tel:{{ $tollFree }}" class=" text-{{ $themeColor }} sm:mb-2 no-decoration tw-text-base">{{ $tollFree }}</a>
-            </div>
-            <div class="tw-flex tw-flex-col tw-w-10/12 sm:tw-w-1/3 tw-font-bold tw-justify-center tw-p-3 tw-border-solid tw-border-0 tw-border-b-2 sm:tw-border-b-0 sm:tw-border-r-2 tw-border-gray-300">
-                <h4 class="tw-font-bold tw-mb-1">Direct/International</h4>
-                <a href="tel:{{ $internationalNumber }}" class=" text-{{ $themeColor }} sm:mb-2 no-decoration tw-text-base">{{ $internationalNumber }}</a>
-            </div>
-            <div class="tw-flex tw-flex-col tw-w-10/12 sm:tw-w-1/3 tw-font-bold tw-justify-center tw-p-3">
-                <h4 class="tw-font-bold tw-mb-1">Office Hours</h4>
-                <p class="text-{{ $themeColor }} tw-text-base"> Monday-Friday</p>
-                <p class="text-{{ $themeColor }} sm:mb-2 tw-text-base">8 AM - 4 PM Pacific Time</p>
-            </div>
+            <h4 class="subtitle">USA & Canada</h4>
+            <a href="tel:{{ $tollFree }}" class="title text-{{ $themeColor }} font-light mb-2 no-decoration">{{ $tollFree }}</a>
+
+            <h4 class="subtitle">Email Address</h4>
+            <a href="mailto:{{ $emailAddress }}" class="title text-{{ $themeColor }} font-light mb-2 no-decoration">{{ $emailAddress }}</a>
         </div>
-    </section>
-
-    <section class="tw-flex tw-flex-col tw-text-center tw-mb-12">
-        <h2 class="tw-font-bold tw-text-3xl tw-mb-3 md:tw-mb-6">Want to join the team?</h2>
-        <p class="tw-text-base">For current available positions at our company, please visit <a href="https://musora.com/jobs" title="go to help center" class="tw-font-bold tw-no-underline text-{{ $themeColor }}">Musora.com/Jobs</a>.</p>
-    </section>
+    </div>
 </div>
