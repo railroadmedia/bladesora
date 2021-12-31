3308c33f07ed85eb3bec4e8e79c038a001fac781
Miguel Fondeur <miguelfondeur@gmail.com>
2021-12-31 21:13:00 +0000

    fix non closing tag in coach footer

--- views/members/components/coach-footer.blade.php
+++ views/members/components/coach-footer.blade.php
@@ -71,8 +71,8 @@
                             }
                         @endphp
                     </div>
+                @endif
             </div>
-            @endif
         </div>
     </div>
 </div>
