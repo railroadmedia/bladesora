9f738c1ae8970e63331c05344b7d2a29fc933596
Yalung Tang <yalungtang@gmail.com>
2022-01-13 21:38:56 +0000

    WIP SONGS URL FIX 404 DO NOT MERGE

--- views/members/components/home/_card-links.blade.php
+++ views/members/components/home/_card-links.blade.php
@@ -7,8 +7,8 @@
         >
             <img class="tw-mx-auto tw-mb-4 tw-block" src="{{ $methodIcon }}"
                 alt="{{ ucfirst($brand) }} Method Icon" />
-            <h3 class="tw-mb-4 tw-text-xl">Step-by-step curriculum.</h3>
-            <p class="tw-mb-4 tw-text-sm">Exclusive curriculum so you'll always know what to work on for maximum results.
+            <h3 class="tw-mb-4 tw-text-xl">{{ $methodTitle }}</h3>
+            <p class="tw-mb-4 tw-text-sm">{{ $methodDescription }}
             </p>
             <a href="{{ $methodUrl }}" title="Go to Methods Page"
                 class="tw-btn tw-btn-primary tw-transition tw-bg-{{ $brand }} tw-px-8 tw-w-full tw-mb-3 hover:tw-bg-{{ $brand }}-600">
@@ -28,10 +28,9 @@
                  src="{{ $songsLogo }}" 
                  alt="Songs Icon" 
             />
-            <h3 class="tw-mb-4 tw-text-xl">Popular songs in all genres.</h3>
-            <p class="tw-mb-4 tw-text-sm">Full transcriptions, loops, and practice tools for music by popular bands of
-                all eras and styles.</p>
-            <a href="/members/lessons/songs" title="Go to Lessons Page"
+            <h3 class="tw-mb-4 tw-text-xl">{{ $songsTitle }}</h3>
+            <p class="tw-mb-4 tw-text-sm">{{ $songsDescription }}</p>
+            <a href="{{ $songsUrl }}" title="Go to Lessons Page"
                 class="tw-btn tw-btn-primary tw-transition tw-px-8 tw-w-full {{ $brand === 'pianote' ? '' : 'tw-bg-pink-600 hover:tw-bg-pink-700' }}"
                 style="{{ $brand === 'pianote' ? 'background-color: rgb(23, 209, 250)' : '' }}"
             >
@@ -43,9 +42,9 @@
     <div class="md:tw-px-1 xl:tw-px-0 tw-mb-2 xl:tw-mb-0 tw-w-full md:tw-w-1/2 xl:tw-w-full">
         <div class="tw-bg-gray-900 tw-h-full tw-bg-cover tw-bg-center tw-text-white tw-text-center tw-rounded-lg tw-p-8" style="background-image: url('{{ imgix($coachCardBg, ['w' => 500]) }} ');">
             <img class="tw-mx-auto tw-mb-4 tw-block" src="{{ $coachCardIcon }}" alt="Coaches Icon" />
-            <h3 class="tw-mb-4 tw-text-xl">Learn with the legends.</h3>
+            <h3 class="tw-mb-4 tw-text-xl">{{ $coachCardTitle }}</h3>
             <p class="tw-mb-4 tw-text-sm">{{ $coachCardDescription }}</p>
-            <a href="/members/coaches" title="Go to Coaches Page"
+            <a href="{{ $coachCardsUrl }}" title="Go to Coaches Page"
                 class="tw-btn tw-btn-primary tw-transition tw-bg-yellow-500 tw-px-8 tw-w-full hover:tw-bg-yellow-600">
                 Go to Coaches <span class="tw-h-full tw-px-1 tw-pb-1">&raquo;</span>
             </a>
