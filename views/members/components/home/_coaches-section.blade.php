18ec9427123277ff02584787ad7ba8ea28b9b860
calebfavor <calebfavor@gmail.com>
2022-01-25 17:43:51 +0000

    update subscribed coaches cards in homepage

--- views/members/components/home/_coaches-section.blade.php
+++ views/members/components/home/_coaches-section.blade.php
@@ -26,21 +26,27 @@
                 @foreach($subscribedCoaches->results() as $coach)
                     <a href="{{ $coach->fetch('url','') }}"
                         class="tw-no-underline ">
-                        <div class="bg-grey-2 tw-rounded-2xl tw-relative tw-h-64 tw-bg-cover tw-overflow-hidden"
+                        <div class="tw-flex tw-bg-cover tw-bg-top tw-h-64 tw-relative tw-bg-gray-200 tw-overflow-hidden tw-rounded-lg lg:tw-rounded-xl tw-no-underline tw-text-white"
                                 style="background-image: url( '{{ $coach->fetch('data.coach_card_image') }}' )"
                         >
-                            <div class="tw-absolute tw-w-full tw-bottom-0 tw-left-0 tw-h-2/3 tw-px-1 tw-py-4 tw-flex tw-flex-col"
-                                    style="background: linear-gradient(180deg, rgba(0, 0, 23, 0) 17.28%, #000017 100%);"
+                            <div class="tw-flex tw-flex-col tw-mt-auto tw-w-full tw-items-center tw-justify-center tw-h-2/3 tw-px-2 tw-text-center"
+                                style="background: linear-gradient(180deg, rgba(1, 5, 15, 0) 0%, #01050F 100%);"
                             >
-                                <h4 class="tw-uppercase tw-mt-auto tw-text-white tw-text-center tw-text-base">
+                                <!-- Coach Name -->
+                                <h4 class="tw-uppercase tw-mt-auto tw-leading-tight tw-text-base tw-mb-4 tw-text-center"
+                                    style="hyphens: auto">
                                     @php
                                         $fullName = $coach->fetch('fields.name');
                                         $exploded = explode(' ', $fullName);
                                         $firstName = array_shift( $exploded );
                                     @endphp
-                                    <span class="tw-font-normal">{{ $firstName }}</span><br>
+                                    <span class="">{{ $firstName }}</span><br>
                                     {{ implode($exploded, ' ')}}
-                                    </h4>
+                                </h4>
+                                <!-- Coach Title -->
+                                <p class="tw-text-yellow-400 tw-text-xs tw-mb-4 md:tw-mb-6 tw-uppercase">
+                                    {{ $coach->fetch('data.focus_text.value') }}
+                                </p>
                             </div>
                             <div class="tw-absolute tw-w-full tw-h-full tw-top-0 tw-left-0 tw-z-10 tw-bg-opacity-0 tw-transition tw-bg-black hover:tw-bg-opacity-25"></div>
                         </div>
